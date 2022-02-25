db36d48f548ee4a970d6afbee1a54cdbeed0a0a8
Rhythm Pandey <78252063+rhyths08@users.noreply.github.com>
2022-02-25 19:17:28 +0530

    Create question6.dart

---
+++ question6.dart
@@ -0,0 +1,28 @@
+#WAP to generate a Fibonacci series up to nth term.
+
+int fibbonacci(int n) 
+{
+   if(n == 0)
+   {
+      return 0;
+   } 
+   else if(n == 1)
+   {
+      return 1;
+   } 
+   else 
+   {
+      return (fibbonacci(n-1) + fibbonacci(n-2));
+   }
+}
+
+void main()
+ {
+   int n=20,i;
+   print("enter the limit of fibonacci series\n");
+   print("Fibbonacci of $n: ");
+   for(i = 0;i<n;i++) 
+   {
+      print("${fibbonacci(i)}  ");            
+   }
+}
