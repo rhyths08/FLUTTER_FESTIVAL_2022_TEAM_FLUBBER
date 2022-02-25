262502d25c66eac5dad3a8f210bb8bb14d089728
KRITI GAUTAM <90100025+Kritiji@users.noreply.github.com>
2022-02-25 12:19:05 +0530

    Create question1.dart

---
+++ question1.dart
@@ -0,0 +1,11 @@
+//Q.1 WAP to check if a number is divisible by 5.
+
+void main() {
+  int n = 20;
+  if(n%2 == 0){
+    print("DIVISIBLE BY 5");
+  }
+  else{
+    print("NOT DIVISIBLE BY 5");
+  }
+}
